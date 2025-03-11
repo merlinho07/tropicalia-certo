ec42b7eaa3b730b622e3ada21e9b69740a295fac
merlinho07 <160855159+merlinho07@users.noreply.github.com>
2025-03-11 09:47:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,4 +17,12 @@ diminuirFonteBotao.addEventListener('click', function(){
     tamanhoAtualFonte -=0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
+
+    const botaoDeAcessibilidade = document. getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');          
+    botaoDeAcessibilidade. addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle ('apresenta-lista');
+    })
+     
 })
